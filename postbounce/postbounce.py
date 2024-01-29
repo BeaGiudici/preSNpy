297025e797ed7daf7392b8edb0bf89b1551de919
beatrice <bea.giudici96@gmail.com>
2024-01-29 10:54:42 +0100

    Adding simple methods

--- postbounce/postbounce.py
+++ postbounce/postbounce.py
@@ -1,4 +1,5 @@
 import h5py
+import numpy as np
 from ..geometry import grid
 from ..hydro import hydro
 from ..nuclear import nuclear
@@ -24,3 +25,41 @@ class Postbounce1D:
 		self.nuclear.fillNuclear()
 
 		# TO-DO: Initialize SCALAR quantities
+
+	def excludeInterior(self):
+		'''
+		Exclude the interior of the star, where the enclused mass is less 
+		than 1.4 Msun.
+		'''
+		return self.grid.mass > 1.4
+	
+	def starMass(self):
+		'''
+		Return the mass of the star.
+		'''
+		return self.grid.mass[-1]
+
+	def starRadius(self):
+		'''
+		Return the radius of the star.
+		'''
+		return self.grid.radius[-1]
+	
+	def compactness(self, masslim=2.5):
+		'''
+		Return the compactness of the star.
+		The compactness is defined as the ratio between the enclosed mass 
+		'masslim' (in unit of Msun) and the radius that encloses that mass 
+		(in unit of 1000 km).
+
+		\begin{equation}
+			\xi_{M} = \left.\frac{M / \Msun}{R(M) / \unit[1000]{km}} 
+								\right|_{t_\mathrm{bounce}}
+		\end{equation}
+		
+		Reference: O'Connor & Ott (2011), Eq. (1)
+		'''
+		idx = np.argmin(np.fabs(self.grid.mass - masslim))
+		rlim = self.grid.radius[idx] / (1.e5) # in km
+		xi = masslim / (rlim/1000)
+		return xi
