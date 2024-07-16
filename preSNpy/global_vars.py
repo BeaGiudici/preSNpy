a67561fb8a01be0e2f8ea0750013bc5ae44a19f2
beatrice <bea.giudici96@gmail.com>
2024-07-16 17:13:45 +0200

    Change in path for dorothy

--- preSNpy/global_vars.py
+++ preSNpy/global_vars.py
@@ -5,6 +5,6 @@ POSTDIR = os.path.join(os.path.expanduser('~'), 'PhD/postbounce_profies/')
 
 global SNMODDEL_DIR
 if os.uname()[1] == 'dorothy':
-  SNMODELS_DIR = os.path.join(os.path.expanduser('~'), 'PhD/1Dsnmodels/')
+  SNMODELS_DIR = os.path.join(os.path.expanduser('~'), '1Dsnmodels/')
 elif os.uname()[1] == 'superviz':
   SNMODELS_DIR = os.path.join(os.path.expanduser('~'), '1Dsnmodels')
