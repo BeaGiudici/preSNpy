6544d1d5ef8e92d508dff3f4062746363a8e3576
Beatrice Giudici <bea.giudici96@gmail.com>
2024-10-15 17:59:50 +0200

    Adding path to plots directory

--- preSNpy/global_vars.py
+++ preSNpy/global_vars.py
@@ -5,7 +5,10 @@ POSTDIR = os.path.join(os.path.expanduser('~'), 'PhD/postbounce_profies/')
 PRESN_DIR = os.path.join(os.path.expanduser('~'), 'PhD/progenitor_models/')
 
 global SNMODDEL_DIR
+global PLOTSDIR
 if os.uname()[1] == 'dorothy':
   SNMODELS_DIR = os.path.join(os.path.expanduser('~'), '1Dsnmodels/')
+  PLOTSDIR = os.path.join(os.path.expanduser('~'), 'PhD', 'plots', 'stellarProgenitors')
 elif os.uname()[1] == 'superviz':
   SNMODELS_DIR = os.path.join(os.path.expanduser('~'), '1Dsnmodels')
+  PLOTSDIR = os.path.join(os.path.expanduser('~'), 'plots', 'stellarProgenitors')
