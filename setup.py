1dcd381e8e1d9ef23e0cde575bd4107851f40ac6
Beatrice Giudici <bea.giudici96@gmail.com>
2024-10-17 09:24:46 +0200

    Adding Pandas to setup.py

--- setup.py
+++ setup.py
@@ -19,6 +19,7 @@ setup(
     install_requires = ['matplotlib',
                       'numpy',                     
                       'scipy',
+                      'pandas',  
                       'h5py',],
     python_requires = '>=3.6',
     classifiers = [
