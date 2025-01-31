65df06b7e59bcec9cc38e4344a93950b4d70f68d
beatrice <bea.giudici96@gmail.com>
2025-01-31 12:46:13 +0100

    Adding log plots

--- preSNpy/physics/physarray.py
+++ preSNpy/physics/physarray.py
@@ -69,10 +69,53 @@ class PhysArray(ndarray):
 		if self.ndim == 1:
 			x, y = _in_grid_units(ax, self.grid.getAxis(axis), self)
 			line, = ax.plot(x.copy(), y.copy(), *args, **kwargs)
-			ax.set_xlabel('%s [%s]' % (axis, x.unit))
-			ax.set_ylabel('%s [%s]' % (y.symbol, y.unit))
+			ax.set_xlabel(r'%s [$\mathrm{%s}$]' % (axis, x.unit))
+			ax.set_ylabel(r'%s [$\mathrm{%s}$]' % (y.symbol, y.unit))
 			if matplotlib.is_interactive() and draw:
 				ax.get_figure().canvas.draw()
 			return line
 		else:
 			raise Exception('Data must be 1-dimensional')
+
+	def plot2D(self, ax, *args, **kwargs):
+		'''
+			Plot 2D model (not useful for now)
+		'''
+		pass
+
+	def plotlogx(self, ax, *args, **kwargs):
+		'''
+			Plot with log scale only on x axis
+		'''
+		import matplotlib
+		line, = self.plot(ax, *args, draw=False, **kwargs)
+
+		ax.set_xscale("log")
+		if matplotlib.is_interactive():
+			ax.get_figure().canvas.draw()
+		return line,
+
+	def plotlogy(self, ax, *args, **kwargs):
+		'''
+			Plot with log scale only on y axis
+		'''
+		import matplotlib
+		line, = self.plot(ax, *args, draw=False, **kwargs)
+
+		ax.set_yscale("log")
+		if matplotlib.is_interactive():
+			ax.get_figure().canvas.draw()
+		return line,
+
+	def plotloglog(self, ax, *args, **kwargs):
+		'''
+			Plot with log scale on both axis
+		'''
+		import matplotlib
+		line, = self.plot(ax, *args, draw=False, **kwargs)
+
+		ax.set_xscale("log")
+		ax.set_yscale("log")
+		if matplotlib.is_interactive():
+			ax.get_figure().canvas.draw()
+		return line,
